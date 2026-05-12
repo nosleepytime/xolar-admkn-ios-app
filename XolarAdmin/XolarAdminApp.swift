da3734a49d55d87149c806f567271b9bbd661f3c
Am I alone… <nosleep.m1tuned@gmail.com>
2026-05-11 20:01:15 -0400

    Initialize XolarAdminApp with SwiftUI structure

---
+++ XolarAdmin/XolarAdminApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct XolarAdminApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
